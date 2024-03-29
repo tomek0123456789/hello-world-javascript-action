08e1a0e898c89862234650b383488b511e96b9cf
Tomasz Miśkowicz <tomasz.miskowicz@swmansion.com>
2024-03-29 14:57:42 +0100

    test building in lib/main.js

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -30513,6 +30513,11 @@ var __webpack_exports__ = {};
 // ESM COMPAT FLAG
 __nccwpck_require__.r(__webpack_exports__);
 
+// EXPORTS
+__nccwpck_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ main)
+});
+
 // EXTERNAL MODULE: ./node_modules/@actions/core/lib/core.js
 var core = __nccwpck_require__(2186);
 // EXTERNAL MODULE: ./node_modules/@actions/exec/lib/exec.js
@@ -30780,9 +30785,6 @@ async function main() {
 
   }
 }
-
-;// CONCATENATED MODULE: ./index.js
-
 
 void main();
 
