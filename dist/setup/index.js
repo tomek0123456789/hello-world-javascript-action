99368f4a97e3d7417e196c1626f383eafaa9c327
Tomasz Miśkowicz <tomasz.miskowicz@swmansion.com>
2024-03-29 13:53:03 +0100

    Formatting

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -1,12 +1,15 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
-/******/ 	/* webpack/runtime/compat */
-/******/ 	
-/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
+require("./sourcemap-register.js");
+/******/ (() => {
+  // webpackBootstrap
+  /******/ /* webpack/runtime/compat */
+  /******/
+  /******/ if (typeof __nccwpck_require__ !== "undefined")
+    __nccwpck_require__.ab = __dirname + "/";
+  /******/
+  /************************************************************************/
+  var __webpack_exports__ = {};
 
-module.exports = __webpack_exports__;
-/******/ })()
-;
+  module.exports = __webpack_exports__;
+  /******/
+})();
 //# sourceMappingURL=index.js.map
